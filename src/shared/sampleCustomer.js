a767548da19c29f5995d38658785cf5d2cae015a
Gavin Palmer <gspalmer@ualberta.ca>
2021-02-17 10:04:28 -0700

    Added Sample Data

---
+++ src/shared/sampleCustomer.js
@@ -0,0 +1,13 @@
+export const SAMPLE_CUSTOMER = [
+    {
+        id: 0,
+        report: '2021-0001',
+        vendor: 'My Company',
+        customer: 'Cenovus',
+        field: 'Christina Lake',
+        well: 'E01P01',
+        install: '2019-01-01T09:00:00.000Z',
+        pull: '2021-01-01T09:00:00.000Z',
+        report: '2021-01-20T09:00:00.000Z'
+    }
+];
---
+++ src/shared/sampleEquipment.js
@@ -0,0 +1,170 @@
+export const SAMPLE_EQUIPMENT= [
+    {
+        id: 0,
+        serialNumber: 'G1234678',
+        description: 'UltraPump 2000',
+        component: 'pump',
+        details: [
+            {
+                part: 'impeller',
+                description: 'Up Thrust',
+                condition: 'Moderate',
+                image: null
+            },
+            {
+                part: 'impeller',
+                description: 'Down Thrust',
+                condition: 'Moderate',
+                image: null
+            },
+            {
+                part: 'impeller',
+                description: 'Radial',
+                condition: 'Moderate',
+                image: null
+            },
+            {
+                part: 'diffuser',
+                description: 'Up Thrust',
+                condition: 'Moderate',
+                image: null
+            },
+            {
+                part: 'diffuser',
+                description: 'Down Thrust',
+                condition: 'Moderate',
+                image: null
+            },
+            {
+                part: 'diffuser',
+                description: 'Radial',
+                condition: 'Moderate',
+                image: null
+            },
+            {
+                part: 'bearing',
+                description: 'Head Bearing',
+                condition: 'Moderate',
+                image: null
+            },
+            {
+                part: 'bearing',
+                description: 'Base Bearing',
+                condition: 'Moderate',
+                image: null
+            },
+            {
+                part: 'shaft',
+                description: 'Head Shaft Splines',
+                condition: 'Moderate',
+                image: null
+            },
+            {
+                part: 'shaft',
+                description: 'Base Shaft Splines',
+                condition: 'Moderate',
+                image: null
+            }
+        ]
+    },
+    {
+        id: 1,
+        serialNumber: 'S456789123',
+        description: 'ChargePump 2500',
+        component: 'pump',
+        details: [
+            {
+                part: 'impeller',
+                description: 'Up Thrust',
+                condition: 'Severe',
+                image: null
+            },
+            {
+                part: 'impeller',
+                description: 'Down Thrust',
+                condition: 'Severe',
+                image: null
+            },
+            {
+                part: 'impeller',
+                description: 'Radial',
+                condition: 'Severe',
+                image: null
+            },
+            {
+                part: 'diffuser',
+                description: 'Up Thrust',
+                condition: 'Severe',
+                image: null
+            },
+            {
+                part: 'diffuser',
+                description: 'Down Thrust',
+                condition: 'Severe',
+                image: null
+            },
+            {
+                part: 'diffuser',
+                description: 'Radial',
+                condition: 'Severe',
+                image: null
+            },
+            {
+                part: 'bearing',
+                description: 'Head Bearing',
+                condition: 'Severe',
+                image: null
+            },
+            {
+                part: 'bearing',
+                description: 'Base Bearing',
+                condition: 'Severe',
+                image: null
+            },
+            {
+                part: 'shaft',
+                description: 'Head Shaft Splines',
+                condition: 'Severe',
+                image: null
+            },
+            {
+                part: 'shaft',
+                description: 'Base Shaft Splines',
+                condition: 'Severe',
+                image: null
+            }
+        ]
+    },
+    {
+        id: 2,
+        serialNumber: 'I987654',
+        description: 'DummyIntake',
+        component: 'intake',
+        details: [
+            {
+                part: 'bearing',
+                description: 'Head Bearing',
+                condition: 'Severe',
+                image: null
+            },
+            {
+                part: 'bearing',
+                description: 'Base Bearing',
+                condition: 'Severe',
+                image: null
+            },
+            {
+                part: 'shaft',
+                description: 'Head Shaft Splines',
+                condition: 'Severe',
+                image: null
+            },
+            {
+                part: 'shaft',
+                description: 'Base Shaft Splines',
+                condition: 'Severe',
+                image: null
+            }
+        ]
+    }
+];
